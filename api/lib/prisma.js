04b514540d783763a0b5f65a3ef976571d84e1aa
Mr-Saransh <saranshagrahari1221@gmail.com>
2026-03-29 20:00:45 +0530

    Fix Prisma 7.6.0 connection configuration

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -2,11 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma;
 
+const options = {
+  datasourceUrl: process.env.DATABASE_URL
+};
+
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient(options);
 } else {
   if (!globalThis._prisma) {
-    globalThis._prisma = new PrismaClient();
+    globalThis._prisma = new PrismaClient(options);
   }
   prisma = globalThis._prisma;
 }
